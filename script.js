0f462ba00d8940f940147b07f55170a289c9cb89
Abhishek kumawat <93272330+abhi0a11@users.noreply.github.com>
2023-07-07 14:08:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+// // Targeting video element 
+// let clip = document.querySelector(".vid-1")
+// // let link=document.getElementById("vid_box_1").innerHTML= `<a  data-aos="flip-up" href="https://abhi0a11.github.io/lt7.00/" target="_blank">`;
+// /* Applying mouseover event on video clip 
+// and then we call play() function to play 
+// the video when the mouse is over the video */
+// clip.addEventListener("mouseover", function (e) {
+//     clip.play();
+// })
+
+// /* Applying mouseout event on video clip 
+// and then we call pause() function to stop 
+// the video when the mouse is out the video */
+// clip.addEventListener("mouseout", function (e) {
+//     clip.pause();
+// })
+
+// onmouseover="this.play()" onmouseout="this.pause();this.currentTime=0;"
+
+
+
+// <!-- <div class=" col-xxl-8 px-4 py-5 cbg c-1  vw-100"> -->
+// <div class=" col-xxl-8 px-4 py-5 cbg c-1  vw-100">
+//     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-3 mx-1">
+// <!-- <div class="row g-3 "> -->
+//     <div class="col">
+//         <!-- <div class="card "> -->
+//             <a href="https://abhi0a11.github.io/lt7.00/" target="_blank">
+//             <!-- <img class="image_img" src="portfolio images/lunchttime.jpg" width="100%" height="225"
+//                 alt=""> -->
+//                 <div class="box box-1 card overflow-hidden"></div>
+//             </a>
+//         <!-- </div> -->
+//         <!-- <a data-aos="flip-up" href="https://abhi0a11.github.io/lt7.00/" target="_blank"
+//             data-aos-easing="ease-out-cubic">
+//             <img src="portfolio images/lunchttime.jpg" class="img-fluid" alt="Lunch time">
+//         </a> -->
+//     </div>
+//     <div class="col">
+//         <div class="card shadow-sm image">
+//             <a href="frontend projects/order-summary/index.html" target="_blank">
+            
+//             </a>
+//         </div>
+        
+//     </div>
+//     <div class="col">
+//         <div class="card shadow-sm image">
+//             <img class="image_img" src="portfolio images/desktop-design.jpg" width="100%" height="225"
+//                 alt="">
+//             <a href="frontend projects/stats-preview-card-component-main/index.html" target="_blank">
+//             </a>
+//         </div>
+//     </div>
+// </div>
+// </div>
+
+// <!-- line 2 -->
+// <div class=" col-xxl-8 px-4 py-5 cbg c-1  vw-100">
+//     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-3 mx-1">
+//         <div class="col">
+//             <div class="card shadow-sm image">
+//                 <img class="image_img" src="portfolio images/lunch_time.jpg" width="100%" height="225" alt="">
+//                 <a href="https://abhi0a11.github.io/lt7.00/" target="_blank">
+//                     <div class="card-body image_overlay">
+//                         <h3 class="card-text image_heading">Lunch Time</h3>
+//                         <!-- <br><br> -->
+//                         <p class="card-text image_preview" style="font-size: 20px;font-weight:700;">
+//                             <img src="portfolio images/binoculars.webp" alt=""
+//                                 data-pagespeed-url-hash="12479754"
+//                                 onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+//                         </p>
+//                     </div>
+//                 </a>
+//                 <div class="card-body">
+//                     <div class="d-flex justify-content-between align-items-center">
+//                         <div class="btn-group">
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//         <div class="col">
+//             <div class="card shadow-sm">
+//                 <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
+//                     xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+//                     preserveAspectRatio="xMidYMid slice" focusable="false">
+//                     <title>Placeholder</title>
+//                     <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
+//                         dy=".3em">Thumbnail</text>
+//                 </svg>
+
+//                 <div class="card-body">
+//                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+//                         additional
+//                         content. This content is a little bit longer.</p>
+//                     <div class="d-flex justify-content-between align-items-center">
+//                         <div class="btn-group">
+//                             <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+//                             <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+//                         </div>
+//                         <small class="text-muted">9 mins</small>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+
+
+
+//     </div>
+// </div>
